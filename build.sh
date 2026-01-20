b06a8bb6f4bcca01b7aee1063f0cc78a072b61d5
Pushpa <pushpa01082020@gmail.com>
2026-01-20 21:18:28 +0530

    Fix deployment path by running Django from backend directory

--- build.sh
+++ build.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
-pip install -r requirements.txt
-python backend/manage.py collectstatic --noinput
-python backend/manage.py migrate
+cd backend
+
+pip install -r ../requirements.txt
+python manage.py collectstatic --noinput
+python manage.py migrate
